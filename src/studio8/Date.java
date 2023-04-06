d4d6a98803201c064935ea3846aacb841e9d5d71
kayle <kayle@DESKTOP-AAN93TJ.dhcp.wustl.edu>
2023-04-06 12:41:44 -0500

    We tried!

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,52 @@
 package studio8;
 
+import java.util.LinkedList;
+
 public class Date {
+	/**
+	 * Instance Variables
+	 * These should be private because we only want to access these in this specific class
+	 */
+	private int month;
+	private int day;
+	private int year;
+	private boolean field;
+	
+	
+	public Date(int month, int day, int year) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.field = field;
+	}
 
+	public String getDate() {
+		return this.month + "/" + this.day + "/"+ this.year;
+	}
+	
+	public boolean getField(boolean field) {
+		if((month == 1) && (day == 1)) {
+			return true;
+		}
+		else if ((month == 12) && (day == 25)) {
+			return true;
+		}
+		else if ((month == 7) && (day == 4)) {
+			return true;
+		}
+		else if ((month == 10) && (day == 31)) {
+			return true;
+		}
+		else {
+			return false;
 
+		}
+	}
+	
     public static void main(String[] args) {
-
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	Date d1 = new Date(month, day, year);
+    
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -3,6 +3,24 @@ package studio8;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean field;
+	
+	public Time(int hour, int minute) {
+		this.hour= hour;
+		this.minute= minute;
+		this.field = field;
+	}
+	
+	public String getTime() {
+		return hour + ":"+ minute;
+	}
+	public getField(boolean field) {
+		if(hour > 12) {
+			
+		}
+	}
 	public static void main(String[] args) {
  
     	
